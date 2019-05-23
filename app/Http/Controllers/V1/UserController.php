115b4f9d353d76b952802beb1318f9b282f4b998
Celso Lopes <celunico43@gmail.com>
2019-05-23 15:51:27 -0300

    WIP -Edit user created

--- app/Http/Controllers/V1/UserController.php
+++ app/Http/Controllers/V1/UserController.php
@@ -13,7 +13,7 @@ class UserController extends BaseController
     public function show()
     {
         $users = User::all();
-        return response()->json($users);
+        return response()->json(['users' => $users]);
     }
 
     public function get($id)
@@ -42,5 +42,25 @@ class UserController extends BaseController
 
         return response()->json(['user' => $user]);
     }
-    
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()->first()]);
+        }
+
+        $user = User::find($id);
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = app('hash')->make($request->input('password'));
+        $user->save();
+
+        return response()->json(['user' => $user]);
+    }
 }
